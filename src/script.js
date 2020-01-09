79432d426881373b1064d6b7ce60cc95f40d1f89
Sourav Goswami <souravgoswami@protonmail.com>
2020-01-09 15:14:35 +0530

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,15 +1,14 @@
 $(() => {
-	var mode = 0
-	var timeCounter = 0
-	var lapCounter = 0
-	var action
-	var lapNumber = 0
-	var startDate = new Date()
-	var timeMinutes, timeSeconds, timeCentiseconds, lapMinutes, lapSeconds, lapCentiseconds
+	var mode = timeCounter = lapCounter = lapNumber = 0
+	var timeDate = stoppedDate = new Date()
+	lapDate = new Date()
+	var lapMinutes, lapSeconds, lapCentiseconds, action
 
 	hideShowButtons('#start-button', '#lap-button')
 	$('#start-button').click(() => {
 		mode = 1
+		timeDate = new Date()
+		lapDate = new Date()
 		hideShowButtons('#stop-button', '#lap-button')
 		startAction()
 	})
@@ -17,10 +16,13 @@ $(() => {
 	$('#stop-button').click(() => {
 		hideShowButtons('#resume-button', '#reset-button')
 		clearInterval(action)
+		stoppedDate = new Date()
 	})
 
 	$('#resume-button').click(() => {
 		hideShowButtons('#stop-button', '#lap-button')
+		timeDate.setMilliseconds( timeDate.getMilliseconds() - (stoppedDate - new Date()) )
+		lapDate.setMilliseconds( lapDate.getMilliseconds() - (stoppedDate - new Date()) )
 		startAction()
 	})
 
@@ -29,8 +31,8 @@ $(() => {
 	$('#lap-button').click(() => {
 		if(mode) {
 			clearInterval(action)
-			lapCounter = 0
 			addLap()
+			lapDate = new Date()
 			startAction()
 		}
 	})
@@ -42,40 +44,45 @@ $(() => {
 		$(y).show()
 	}
 
+	function fmt2(arg, len = 3) {
+		argLength = arg.toString().length
+		return argLength < len ? '0'.repeat(len - argLength) + arg : arg
+	}
+
 	function startAction() {
-		startDate = new Date()
 		action = setInterval(() => {
-			lapMinutes = Math.floor(lapCounter / 6000)
-			lapSeconds = Math.floor(lapCounter % 6000 / 100)
-			lapCentiseconds = lapCounter % 6000 % 100
+			let currentDate = new Date()
+
+			lapCounter = currentDate - lapDate
+			timeCounter = currentDate - timeDate
+
+			lapMinutes = Math.floor(lapCounter / 60000)
+			lapSeconds = Math.floor(lapCounter % 60000 / 1000)
+			lapCentiseconds = lapCounter % 60000 % 1000
 
 			$('#lap-minute').text(lapMinutes < 10 ? '0' + lapMinutes : lapMinutes)
 			$('#lap-second').text(lapSeconds < 10 ? '0' + lapSeconds : lapSeconds)
-			$('#lap-centisecond').text(lapCentiseconds < 10 ? '0' + lapCentiseconds : lapCentiseconds)
+			$('#lap-centisecond').text(fmt2(lapCentiseconds))
 
-			timeMinutes = Math.floor(timeCounter  / 6000)
-			timeSeconds = Math.floor(timeCounter % 6000 / 100)
-			timeCentiseconds = timeCounter % 6000 % 100
+			let timeMinutes = Math.floor(timeCounter  / 60000)
+			let timeSeconds = Math.floor(timeCounter % 60000 / 1000)
+			let timeCentiseconds = timeCounter % 60000 % 1000
 
 			$('#time-minute').text(timeMinutes < 10 ? '0' + timeMinutes : timeMinutes)
 			$('#time-second').text(timeSeconds < 10 ? '0' + timeSeconds : timeSeconds)
-			$('#time-centisecond').text(timeCentiseconds < 10 ? '0' + timeCentiseconds : timeCentiseconds)
-
-			timeCounter += 1
-			lapCounter += 1
+			$('#time-centisecond').text(fmt2(timeCentiseconds))
 		}, 10)
 	}
 
 	function addLap() {
 		fmt = arg => arg < 10 ? '0' + arg : arg
-		fmt2  = (arg, len = 3) => arg.length < len ? '0'.repeat(len - arg.length) + arg : arg
 
-		date = new Date()
-		var h = fmt(date.getHours()), m = fmt(date.getMinutes()), s = fmt(date.getSeconds())
-		var sH = fmt(startDate.getHours()), sM = fmt(startDate.getMinutes()), sS = fmt(startDate.getSeconds())
-		var mS = fmt2(date.getMilliseconds().toString()), sMS = fmt2(startDate.getMilliseconds().toString())
+		let date = new Date()
+		let h = fmt(date.getHours()), m = fmt(date.getMinutes()), s = fmt(date.getSeconds())
+		let sH = fmt(lapDate.getHours()), sM = fmt(lapDate.getMinutes()), sS = fmt(lapDate.getSeconds())
+		let mS = fmt2(date.getMilliseconds()), sMS = fmt2(lapDate.getMilliseconds())
 
-		var myLapDetails = `<div class="lap"><div class="laptime-title">Lap${++lapNumber}</div>` +
+		let myLapDetails = `<div class="lap"><div class="laptime-title">Lap${++lapNumber}</div>` +
 			`<div class="system-time">(${sH}:${sM}:${sS}:${sMS} - ${h}:${m}:${s}:${mS})</div>` +
 			'<div class="laptime">' +
 				'<span>' + fmt(lapMinutes) + '</span>:' +
